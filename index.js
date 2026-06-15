4ac2d8089aadfb8f63d6d82fd422040b2a3ca24a
Abhinav Kumar <kumarabhinav1908@gmail.com>
2026-06-15 10:49:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,106 @@
+// console.log("Abhinav");
+
+// Arithmetic Operator
+
+// let a = 10;
+// let b = 5;
+
+// console.log(a**b);
+
+// let a = 2;
+// let b = 5;
+
+//  a = a + 10;
+// a += 10;
+// console.log(a);
+
+//  a = a - 2;
+// a -= 2;
+// console.log(a);
+
+// let a = 2;
+// let b = 5;
+
+// console.log(50<=5);
+
+// console.log('5' == 5);
+// console.log('5' === 5);
+
+// console.log(5 != "5");
+// console.log(5 !== "5");
+
+// let age = 5;
+// let status1 = (age > 18) ? 'I can vote' : "I cannot vote";
+
+
+// console.log(status1);
+
+// let ans = (true && false &&true)
+
+// let ans = (false || false || false);
+
+// let ans = !(false);
+// console.log(ans);
+
+// console.log(false || 7 || 11 || 18);
+
+// console.log(2 & 5);
+// console.log(2 | 5);
+
+// console.log(~(0));
+
+// console.log(2^2);
+
+// console.log(10 >> 3);
+
+// let age = 3;
+// if(age >= 18) {
+//     console.log('can vote');
+// }
+// else {
+//     console.log('cannot vote');
+// }
+
+// let age = 30;
+// if(age >= 18) {
+//     console.log('can vote');
+// }
+
+// let number = 4;
+// let age = 104;
+
+
+// if(number == 1) {
+//     console.log('A');
+// }
+// else if(number == 2) {
+//     console.log('B');
+// }
+// else if(number == 3) {
+//     console.log('C');
+// }
+// else if(number == 4) {
+//     if(age > 18) {
+//         console.log('I can vote');
+//     }
+//     console.log('D');
+// }
+// else {
+//     console.log('F');
+// }
+
+// let num = 3;
+
+// switch(num) {
+//     case 1: console.log('A');
+//     break;
+//     case 2: console.log('B');
+//     break;
+//     case 3: console.log('C');
+//     break;
+//     case 4: console.log('D');
+//     break;
+//     default: console.log('F'); 
+// }
+
+
